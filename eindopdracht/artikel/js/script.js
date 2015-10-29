70339c564e1704e6a5b0c357a37f55ea114b8a96
nooroel <Nooroel@042.191.028.145.hva.nl>
2015-10-29 11:40:02 +0100

    Eindopdracht

--- eindopdracht/artikel/js/script.js
+++ eindopdracht/artikel/js/script.js
@@ -1,5 +1,7 @@
 var currentPage = document.querySelector('nav ul li:nth-of-type(1) a');
 var searchBox = document.querySelector('body header form')
 
+
 currentPage.classList.add('activeLink');
 searchBox.style.display = ("none");
+
--- eindopdracht/formulier/js/script.js
+++ eindopdracht/formulier/js/script.js
@@ -1,2 +1,22 @@
 var currentPage = document.querySelector('nav ul li:nth-of-type(1) a');
+var projectForm = document.getElementById('project');
+var stageForm = document.getElementById('stage');
+var projectBtn = document.querySelector('input[value=project]');
+var stageBtn = document.querySelector('input[value=stage]');
+
+
 currentPage.classList.add('activeLink');
+
+projectForm.classList.add('hide');
+stageForm.classList.add('hide');
+
+projectBtn.addEventListener('click', function(){
+    projectForm.classList.remove('hide');
+    formStage.classList.add('hide');
+});
+
+stageBtn.addEventListener('click', function(){
+    projectForm.classList.add('hide');
+    stageForm.classList.remove('hide');
+});
+
