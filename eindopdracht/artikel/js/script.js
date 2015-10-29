af6b73c84277033f9c11434e9b98ee7d4bfae30c
nooroel <Nooroel@MacBook-Pro-van-Nooroel.local>
2015-10-29 09:32:57 +0100

    Formulier

--- eindopdracht/artikel/js/script.js
+++ eindopdracht/artikel/js/script.js
@@ -1,2 +1,5 @@
 var currentPage = document.querySelector('nav ul li:nth-of-type(1) a');
+var profiel = document.querySelector('body header p')
+
 currentPage.classList.add('activeLink');
+profiel.classList.remove('to-profile');
