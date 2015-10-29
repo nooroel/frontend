947d43b8d7f8637c81892736f0a8f83c710d51b6
nooroel <Nooroel@MacBook-Pro-van-Nooroel.local>
2015-10-29 10:27:37 +0100

    Eindopdracht

--- eindopdracht/artikel/js/script.js
+++ eindopdracht/artikel/js/script.js
@@ -2,4 +2,4 @@ var currentPage = document.querySelector('nav ul li:nth-of-type(1) a');
 var searchBox = document.querySelector('body header form')
 
 currentPage.classList.add('activeLink');
-searchBox.style.display = ("none")
+searchBox.style.display = ("none");
